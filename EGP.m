62a6b8fb56935cf63c5479f2b14edad04206d87a
martin <no.martini.no@gmail.com>
2015-01-22 16:54:27 +0100

    added "copy" method for EGP model

--- EGP.m
+++ EGP.m
@@ -262,7 +262,28 @@ methods
 		end
 		[~,sid]=sort(s,'descend');
 		informationGain=informationGain(sid,:);		
-	end
+    end
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %utilities:
+    %
+    %
+    function new = copy(this)
+        % Instantiate new object of the same class.
+        new = feval(class(this));
+
+        % Copy all non-hidden properties.
+        p = properties(this);
+        for i = 1:length(p)
+            new.(p{i}) = this.(p{i});
+        end
+    end        
+    
+    
+    %
+    %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
 
 
